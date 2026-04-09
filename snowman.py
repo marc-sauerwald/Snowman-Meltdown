f6f920e54da31a12842999c8f1ab68d826d1b06e
marc-sauerwald <sauerwald.marc@gmail.com>
2026-04-09 16:31:35 +0200

    Update entry point with docstring and main() call

--- snowman.py
+++ snowman.py
@@ -1,4 +1,13 @@
-from game_logic import play_game
+"""
+Snowman Meltdown - A Word Guessing Game
+
+Save the snowman by guessing the secret word before it melts!
+
+Author: Your Name
+Version: 2.0
+"""
+
+from game_logic import main
 
 if __name__ == "__main__":
-    play_game()
+    main()
